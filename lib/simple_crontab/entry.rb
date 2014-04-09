beb35ad20a2c7c45e0412c4470292c4739663944
Marek Aufart <aufi.cz@gmail.com>
2014-04-09 21:48:01 +0200

    adding entry class to represent one crontab line

--- lib/simple_crontab/entry.rb
+++ lib/simple_crontab/entry.rb
@@ -1,6 +1,22 @@
 module SimpleCrontab
 
   class Entry
+    attr_accessor :rule, :command
+
+    def initialize(rule = '', command = '')
+      @rule = rule
+      @command = command
+    end
+
+    def serialize
+      "#{rule}  #{command}" if valid?
+    end
+
+    private
+
+    def valid?
+      !rule.to_s.empty? && !command.to_s.empty? #TODO: raise some error?
+    end
 
   end
 
--- spec/lib/entry_spec.rb
+++ spec/lib/entry_spec.rb
@@ -2,14 +2,28 @@ require 'spec_helper.rb'
 
 describe SimpleCrontab::Entry do
 
-  subject { SimpleCrontab::Entry.new }
+  subject { SimpleCrontab::Entry.new(rule = '0 0 * * *', command = '/usr/bin/date') }
 
   describe 'can instantiate and respond to basic methods' do
 
-    pending 'respond to handle methods' do
-      expect(subject.respond_to? :list).to be true
-      expect(subject.respond_to? :save).to be true
-      expect(subject.respond_to? :delete).to be true
+    it 'respond to basic api methods' do
+      expect(subject.respond_to? :serialize).to be true
+      expect(subject.respond_to? :rule).to be true
+      expect(subject.respond_to? :command).to be true
+    end
+
+    describe 'serialize' do
+
+      it 'valid entry' do
+        expect(subject.serialize).to eq '0 0 * * *  /usr/bin/date'
+      end
+
+      it 'invalid entry' do
+        subject.rule = ''
+        subject.command = ''
+        expect(subject.serialize).to eq nil
+      end
+
     end
 
   end
